de2acedfdce8c05bd07b493e5d402379db0fb87b
karen <hoyoszuluagak@gmail.com>
2025-10-21 09:29:04 -0500

    Cors

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://127.0.0.1:5500", "http://localhost:5500", "https://saturday-wrzv.onrender.com/"],
+  origin: ["http://127.0.0.1:5500", "http://localhost:5500", "https://saturday-flame.vercel.app"],
   credentials: true
 }));
 
